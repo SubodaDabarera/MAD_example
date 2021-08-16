1a06b564f5aa647d070653fbd702013ac9376cbd
[SubodaDabarera] <[Subodadabarera99@gmail.com]>
2021-08-16 23:29:14 +0530

    sample data

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    //my value
+    public static final String EXTRA_MESSAGE = "myFirst.Message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.et_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message); startActivity(intent);
+
+    }
+}
